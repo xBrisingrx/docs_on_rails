d06c3d2b9a092a98df39553055654c59ff47b8c2
Brisingr <mdavid.almiron@gmail.com>
2022-12-22 20:46:02 -0300

    add function for add mesagges success or error in forms

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -10,3 +10,32 @@ function noty_alert(type, message, time = 7000) {
     closeWith: ['click', 'button'],
   }).show();
 }
+
+function set_input_status_form(form_id, object_name){
+  // form_id es el id del form
+  // object_name es el nombre del modelo 
+  let inputs = document.querySelectorAll(`#${form_id} .form-control`)
+  let list_ids = []
+  for (let i = 0; i< inputs.length; i++) {
+      list_ids[i] = inputs[i].id
+  }
+  $.each( msj, function( key, value ) {
+    $(`#${form_id} #${object_name}_${key}`).addClass('is-invalid')
+    $(`#${form_id} .${object_name}_${key}`).text( value ).show('slow')
+    list_ids = list_ids.filter( element => element != `${object_name}_${key}`)
+  })
+
+  for (let i = list_ids.length - 1; i >= 0; i--) {
+    let input = document.querySelector(`#${form_id} #${list_ids[i]}`)
+    input.classList.remove('is-invalid')
+    document.querySelector(`#${form_id} .${list_ids[i]}`).textContent = ''
+
+    if ( input.value != '' ) {
+      input.classList.add('is-valid')
+    }
+    
+    if ( input.value == '' ) {
+      input.classList.remove('is-valid')
+    }
+  }
+}

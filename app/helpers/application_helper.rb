290dee7e27cd3d5a7b4c107e627f6636fdb76e0b
Mauro Almiron <mdavid.almiron@gmail.com>
2022-12-26 12:05:34 -0300

    add: format to status
    I use this field in multiple tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
 	def date_format date
 		(date) ? date.strftime('%d-%m-%y') : ''
 	end
+
+	def status_format status
+		( status ) ? '<p class="u-tags-v1 g-color-green g-brd-around g-brd-green g-bg-green-opacity-0_1 g-bg-green--hover g-color-white--hover g-py-2 g-px-5">Activo</p>' 
+		: '<p class="u-tags-v1 g-color-pink g-brd-around g-brd-pink g-bg-pink-opacity-0_1 g-bg-pink--hover g-color-white--hover g-py-2 g-px-5">Inactivo</p>'
+	end
 end

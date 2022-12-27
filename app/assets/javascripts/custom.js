9d0413ccfb433affcf0626353d49f02fd2e77f32
Mauro Almiron <mdavid.almiron@gmail.com>
2022-12-27 18:02:13 -0300

    fix: error messages in forms

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -16,19 +16,32 @@ function set_input_status_form( form_id, object_name, msg ){
   // object_name es el nombre del modelo 
   let inputs = document.querySelectorAll(`#${form_id} .form-control`)
   let list_ids = []
+  let error_msg
   for (let i = 0; i< inputs.length; i++) {
       list_ids[i] = inputs[i].id
   }
   $.each( msg, function( key, value ) {
+    if ( value.length > 1 ) {
+      error_msg = "<ul class='pl-2'>"
+      value.forEach(element =>  error_msg += `<li>${element}</li>` );
+      error_msg += '</ul>'
+    } else {
+      error_msg = value[0]
+    }
+
     $(`#${form_id} #${object_name}_${key}`).addClass('is-invalid')
-    $(`#${form_id} .${object_name}_${key}`).text( value ).show('slow')
+    document.querySelector(`#${form_id} .${object_name}_${key}`).innerHTML = error_msg
     list_ids = list_ids.filter( element => element != `${object_name}_${key}`)
   })
 
   for (let i = list_ids.length - 1; i >= 0; i--) {
     let input = document.querySelector(`#${form_id} #${list_ids[i]}`)
     input.classList.remove('is-invalid')
-    document.querySelector(`#${form_id} .${list_ids[i]}`).textContent = ''
+    if (document.querySelector(`#${form_id} .${list_ids[i]}`) == null) {
+      console.log('!!!!!esto es nulo => ',`#${form_id} .${list_ids[i]}`)
+    } else {
+      document.querySelector(`#${form_id} .${list_ids[i]}`).textContent = ''
+    }
 
     if ( input.value != '' ) {
       input.classList.add('is-valid')

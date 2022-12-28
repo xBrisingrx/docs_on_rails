7b9507c2b05c0014cc422094970c9e78f6ee860e
Brisingr <mdavid.almiron@gmail.com>
2022-12-28 14:27:51 -0300

    add method to open disable modal in document_profile controller

--- app/controllers/documents_profiles_controller.rb
+++ app/controllers/documents_profiles_controller.rb
@@ -1,5 +1,5 @@
 class DocumentsProfilesController < ApplicationController
-  before_action :set_documents_profile, only: %i[ show edit update destroy ]
+  before_action :set_documents_profile, only: %i[ show edit update modal_disable ]
 
   def index
     @documents_profiles = DocumentsProfile.where(d_type: params[:d_type])
@@ -16,6 +16,9 @@ class DocumentsProfilesController < ApplicationController
   end
 
   def edit
+    @title_modal = 'Editar'
+    @profiles = Profile.where( d_type: @documents_profile.d_type)
+    @documents = Document.where( d_type: @documents_profile.d_type)
   end
 
   def create
@@ -34,8 +37,8 @@ class DocumentsProfilesController < ApplicationController
 
   def update
     respond_to do |format|
-      if @documents_profile.update(documents_profile_params)
-        format.json {  render json: { status: :success, msg: 'Asignación actualidaza.' }, status: :ok, location: @documents_profile }
+      if @documents_profile.update(document_profile_update_params)
+        format.json {  render json: { status: :success, msg: 'Datos actualizados.' }, status: :ok, location: @documents_profile }
         format.html { redirect_to documents_profile_url(@documents_profile), notice: "Documents profile was successfully updated." }
       else
         format.json { render json: @documents_profile.errors, status: :unprocessable_entity }
@@ -44,12 +47,17 @@ class DocumentsProfilesController < ApplicationController
     end
   end
 
-  def destroy
-    @documents_profile.destroy
+  def modal_disable;end
+
+  def disable
+    @document_profile = DocumentsProfile.find(params[:document_profile_id])
 
     respond_to do |format|
-      format.html { redirect_to documents_profiles_url, notice: "Documents profile was successfully destroyed." }
-      format.json { head :no_content }
+      if @document_profile.update(active: false, end_date: params[:end_date])
+        format.json { render json: { msg: 'Operacion exitosa', status: 'success' }, status: :ok }
+      else
+        format.json { render json: { msg: 'Ocurrio un error al realizar la operacion' }, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -61,5 +69,9 @@ class DocumentsProfilesController < ApplicationController
 
     def documents_profile_params
       params.require(:documents_profile).permit(:profile_id, :document_id, :start_date, :end_date, :d_type)
+    end
+
+    def document_profile_update_params
+      params.require(:documents_profile).permit(:start_date)
     end
 end

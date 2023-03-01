04a7f34b291149de50d86c4d7e6edcf07e0e73a2
Brisingr <mdavid.almiron@gmail.com>
2023-03-01 12:27:53 -0300

    para habilitar y deshabilitar usamos la ruta edit
    el estado es el unico dato q se puede modificar

--- config/routes.rb
+++ config/routes.rb
@@ -43,11 +43,12 @@ Rails.application.routes.draw do
   resources :documents_profiles, except: [:destroy, :show]
   post 'disable_document_profile', to: 'documents_profiles#disable', as: 'disable_document_profile'
   
-  resources :people_profiles, except: [:destroy, :show] do 
-    get 'modal_disable'
-  end
+  resources :people_profiles, except: [:destroy, :show] 
+
   resources :assignments_profiles, except: [:index, :destroy]
   post 'disable_assignment_profile', to: 'assignments_profiles#disable', as: 'disable_assignment_profile'
+  post 'enable_assignment_profile', to: 'assignments_profiles#enable', as: 'enable_assignment_profile'
+
   resources :assignments_documents, except: [:index, :destroy]
   get 'assignments_documents/:id/modal_disable', to: 'assignments_documents#modal_disable', as: 'modal_disable_assignments_documents'
   post 'disable_assignments_documents', to: 'assignments_documents#disable', as: 'disable_assignments_documents'

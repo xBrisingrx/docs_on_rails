e218c38763acdb2faae34adea5b2b419b6d2575a
Mauro Almiron <mdavid.almiron@gmail.com>
2023-04-01 19:08:28 -0300

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,8 @@ Rails.application.routes.draw do
   get 'main/welcome'
 
   namespace :authentication, path: '', as: '' do
-    resources :users, only: [:index,:new, :create]
+    resources :users, only: [:index,:new, :create, :edit, :update]
+      post 'disable_user', to: 'users#disable', as: 'disable_user'
     resources :sessions, only: [:create]
     get 'login', to: 'sessions#new', as: 'login'
     get 'logout', to: 'sessions#destroy', as: 'logout'

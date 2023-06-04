048ca03b243f1a65f7d8e56dd269c1e93a7949a0
Mauro Almiron <mdavid.almiron@gmail.com>
2023-06-04 17:02:06 -0300

    add description to model

--- app/models/assignation_status.rb
+++ app/models/assignation_status.rb
@@ -12,7 +12,10 @@
 #  updated_at  :datetime         not null
 #
 class AssignationStatus < ApplicationRecord
-
+	# son los estados de las asignaciones
+	# hay estados que se pueden compartir, eso depende del atributo blocks
+	# Ej. si una unidad esta asignada a mantenimiento, en esas fechas no puede ser asignada a otro lugar
+	
 	validates :name, presence: true, 
 		uniqueness: { scope: :d_type, case_sensitive: false, message: "Este estado ya se encuentra registrado" }
 	validates :d_type, presence: true
